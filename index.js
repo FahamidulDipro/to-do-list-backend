db9f864f6d2c484ca9e73c8a168390a263da5232
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-07-01 05:28:20 +0600

    completed task insert and delete from completed task api created

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ async function run() {
   try {
     await client.connect();
     const toDoCollection = client.db("to-do-list").collection("to-do-items");
+    const completedTaskCollection = client
+      .db("to-do-list")
+      .collection("completed-tasks");
     console.log("DB connected!");
 
     //Displaying all to-dos
@@ -60,6 +63,26 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const deletedTask = await toDoCollection.deleteOne(query);
       res.send(deletedTask);
+    });
+    //Checkbox insert
+    app.post("/completedTasks", async (req, res) => {
+      const task = req.body;
+      console.log(task);
+      const insertedTask = await completedTaskCollection.insertOne(task);
+      res.send(insertedTask);
+    });
+    //Deleting Task from completed
+    app.delete("/deleteFromCompleted/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const deletedTask = await completedTaskCollection.deleteOne(query);
+      res.send(deletedTask);
+    });
+    //Loading from Completed Tasks
+    app.get("/completedTasks", async (req, res) => {
+      const completedTasks = await completedTaskCollection.find().toArray();
+      res.send(completedTasks);
     });
   } finally {
   }
