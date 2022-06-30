5ef15a5dd88a95e58c30e268eb6ba9179943971b
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-06-30 18:10:51 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
@@ -12,3 +13,19 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+//Connecting to Database
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.etm3opb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    console.log("DB connected!");
+  } finally {
+  }
+}
+run().catch(console.dir);
